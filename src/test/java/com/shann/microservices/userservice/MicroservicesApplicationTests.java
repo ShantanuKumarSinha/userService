1187dc3cb0ca619a103a69b45dfe0c4352bfa0fd
Shantanu Kumar <shan.raj93@gmail.com>
2026-05-19 01:11:01 +0530

    init commit

---
+++ src/test/java/com/shann/microservices/userservice/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shann.microservices.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
